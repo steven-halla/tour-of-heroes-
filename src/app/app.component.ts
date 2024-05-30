19e01163f6ff1cc67c96da2573c826798b6d2c95
steven-halla <steven.r.halla@gmail.com>
2024-05-29 17:09:14 -0700

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HeroesComponent} from "../heroes/heroes.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  // imports: [RouterOutlet],
   templateUrl: './app.component.html',
+  imports: [
+    HeroesComponent
+  ],
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-tour-of-heroes';
+  title = 'Tour Of Heroes';
 }
